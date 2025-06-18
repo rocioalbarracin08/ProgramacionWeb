4d6e5133cb0b229224293ab624de1d86e642dcb7
Rocio <ralbarracintrab@gmail.com>
2025-06-18 16:52:11 -0300

    Tema nuevo: React, procto con React y funciones

---
+++ React/primer_proyecto_react/src/components/proveedor/proveedor.tsx
@@ -0,0 +1,8 @@
+export function Proveedor({name}){ //nombre de la clave en llaves "{}"
+    return(
+        <> 
+            export nombrado
+            <p>{name}</p> 
+        </>
+    ) // Usamos llave para que no lo tome como texto plano
+};
---
+++ React/primer_proyecto_react/src/components/usuario/Usuario.tsx
@@ -0,0 +1,7 @@
+function Usuario(){
+    return(
+        <>Este es un componente</> //Puedo usar este componente en otro lado: Si -> exportando (1ro con default)
+    )
+}
+export default Usuario 
+//Tiene que tener la misma extención que en el archivo que se va importar
