5671bb13846625509575b594952e91f9f8d3317f
Rocio <ralbarracintrab@gmail.com>
2025-09-03 17:12:50 -0300

    Realizamos ejercicios para obtener 10points. Seguimos con useState y ejercicios para ponerlo en práctica

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ npm install: para instalar dependencias
 Spread operator
 
 Como hay eventos, a travez del onClick o todos los eventos que empiezan con on disponibles en React
+
--- React/prev-state-spread-operator/src/App.jsx
+++ React/prev-state-spread-operator/src/App.jsx
@@ -10,13 +10,19 @@ function App() {
     setNum([...num, num.length +1]); //num es el nombre del arreglo | Los "..." son el contenido anterior (de la lista) | El 5 es agregado a lo ultimo de la lista
   }
   function agregarPropiedad() {
-    setPersona({...persona, edad:17}) //Ponemos "{}" por ser un objeto
-    console.log(persona)
+    const prop = {...persona, edad:17}
+    setPersona(prop) //Ponemos "{}" por ser un objeto
+    console.log(prop) //De forma SINCRÓNICA
+  }
+  function cambiarNombre(){
+    setPersona({...persona, nombre:"Luis"}); //Segunda forma sin usar variable a parte
+    console.log(persona) //De forma asincrónica
   }
 
   //TAREA: Hacer que el setNum agregue números consecutivos
   //Agregar una propiedad(clave/valor) al objeto persona usando spread operator
 
+  //Partiendo del nombre pepito edad 30 cambiar solo nombre a Luis usando Spread
 
   //CONCEPTUALMENTE: un arreglo no es lo mismo que un objeto. En este ejemplo el objeto no se mapea
 
@@ -29,6 +35,9 @@ function App() {
 
         <button onClick= {myFunction} >Agregar número</button>
         <button onClick= {agregarPropiedad} >Agregar persona</button>
+        <button onClick= {cambiarNombre}>Cambiar nombre</button>
+
+        <p>Nombre: {persona.nombre}</p>
       </div>
     </>
   )
